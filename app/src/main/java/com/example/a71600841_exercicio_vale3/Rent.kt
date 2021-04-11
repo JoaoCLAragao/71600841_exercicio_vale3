5bf6b8a10c21c09d689475da41b4d91b10d0658e
JoaoCLAragao <71600841@aluno.faculdadecotemig.br>
2021-04-11 20:10:50 -0300

    Fim do Item_Lista e criação da classe Rent

---
+++ app/src/main/java/com/example/a71600841_exercicio_vale3/Rent.kt
@@ -0,0 +1,12 @@
+package com.example.a71600841_exercicio_vale3
+
+data class Rent(
+        var value_rent: Double,
+        var star: Int,
+        var adress: String,
+        var time: String,
+        var photo_user: Int,
+        var photo_owner: Int,
+        var name: String,
+        var surname: String
+)
