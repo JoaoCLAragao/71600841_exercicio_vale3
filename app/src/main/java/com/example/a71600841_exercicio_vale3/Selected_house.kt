c6bb2c95fee9e89fa9299cf4762cda9f8e1111a1
JoaoCLAragao <71600841@aluno.faculdadecotemig.br>
2021-04-12 01:24:56 -0300

    Criação segunda tela.

---
+++ app/src/main/java/com/example/a71600841_exercicio_vale3/Selected_house.kt
@@ -0,0 +1,11 @@
+package com.example.a71600841_exercicio_vale3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Selected_house : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_selected_house)
+    }
+}
